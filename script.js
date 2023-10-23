92a096aec62605b3351fc576f5f195733499ad68
N UMESH <102941752+Umesh8142@users.noreply.github.com>
2023-10-23 15:25:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+function delColor() {
+  const select = document.getElementById("colorSelect");
+  let selectedValue = select.options[select.selectedIndex].value;
+
+  for (var i = 0; i < select.options.length; i++) {
+    if (select.options[i].value === selectedValue) {
+      select.remove(i);
+      break; // Exit the loop once the option is removed
+    }
+  }
+}
